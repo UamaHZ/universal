e5cbd6701193ff9bf5a75b375e94f8122b290bde
liwei <liwei@uama.com.cn>
2016-12-28 15:39:35 +0800

    增加 ToastUtils

---
+++ uama-utils/src/main/java/com/uama/utils/ToastUtil.java
@@ -0,0 +1,62 @@
+package com.uama.utils;
+
+import android.content.Context;
+import android.support.annotation.StringRes;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+    static Toast toast = null;
+
+    public static void show(Context context, String info) {
+        if (null == toast) {
+            toast = Toast.makeText(context, info, Toast.LENGTH_SHORT);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(info);
+        }
+        toast.show();
+//		Toast.makeText(context, info, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void show(Context context, @StringRes int info) {
+        if (null == toast) {
+            toast = Toast.makeText(context, info, Toast.LENGTH_SHORT);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(info);
+        }
+        toast.show();
+    }
+
+    public static void showLong(Context context, String info) {
+        if (null == toast) {
+            toast = Toast.makeText(context, info, Toast.LENGTH_LONG);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(info);
+        }
+        toast.show();
+    }
+
+    public static void showLong(Context context, @StringRes int info) {
+        if (null == toast) {
+            toast = Toast.makeText(context, info, Toast.LENGTH_LONG);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(info);
+        }
+        toast.show();
+//		Toast.makeText(context, info, Toast.LENGTH_LONG).show();
+    }
+
+    public static void showMaxLengthTip(Context context, int size) {
+        if (null == toast) {
+            toast = Toast.makeText(context, "不能超过" + size + "字", Toast.LENGTH_LONG);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText("不能超过" + size + "字");
+        }
+        toast.show();
+    }
+}
